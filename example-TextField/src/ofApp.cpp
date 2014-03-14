112f77ba61bce31fe3b145321e48bd38c6bdd280
wasawi <criccrec@gmail.com>
2014-03-14 23:27:41 +0100

    added gitignore and renamed testApp to ofApp

--- example-TextField/src/ofApp.cpp
+++ example-TextField/src/ofApp.cpp
@@ -1,79 +1,74 @@
-#include "testApp.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
-    
-   
-    
-   
-
+void ofApp::setup(){
     
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-
+void ofApp::update(){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-   
+void ofApp::draw(){
+	
     text.draw(100,100, 100, 40);
     text2.draw(100,145, 100, 40);
     text3.draw(100,190, 100, 40);
-
+	
     
     ofDrawBitmapString("Result: "+text.getText()+text2.getText()+text3.getText(), 50,250);
     
-
-
+	
+	
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseMoved(int x, int y ){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mouseDragged(int x, int y, int button){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button){
     
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
+void ofApp::windowResized(int w, int h){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
+void ofApp::gotMessage(ofMessage msg){
+	
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+	
 }
-void testApp::exit(){
+void ofApp::exit(){
     
-   
+	
 }
